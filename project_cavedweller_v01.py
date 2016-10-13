dbce19ca301762929a55237ff71cb0637be7fe68
BearTactix <beartactix@gmail.com>
2016-10-13 14:05:04 -0400

    Add files via upload

---
+++ project_cavedweller_v01.py
@@ -0,0 +1,24 @@
+#Project Cave Dweller
+#------------------------------------------------------
+#MAKE SURE YOU CHANGE THE VERSION EVERYTIME YOU EDIT IT 
+#Version: 0.01
+#Date Edited: September 12 2016
+#------------------------------------------------------
+#Adam, Andres, Antonio Karlo, Peter, Seymour
+#------------------------------------------------------
+
+#Imports pygame and other necessities 
+import pygame, random, sys 
+from pygame.locals import *
+
+#Sets up the pygame variables and requirements 
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+TEXTCOLOR = (255,255,255)
+BACKGROUNDCOLOR = (0,0,0)
+FPS = 60
+REQUIREDFOOD = 50
+STARTINGFOOD = 0
+PLAYERMOVERATE = 6
+
+#Sets up the main functions for the game
